34a4ad620cafec5af0cfa3dd3a7fc280d8f3e7ad
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-05-13 09:57:17 -0500

    Made some changes to get the users microservice working through api gateway

--- API/api.js
+++ API/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
+
 const PORT = 3000;
 
 app.get('/service-catalog', async (req, res) => {
@@ -24,15 +26,33 @@ app.get('/service-scheduling', async (req, res) => {
     }
 });
 
-app.get('/service-user-management', async (req, res) => {
+app.get('/test2', async (req, res) => {
     try {
-        const response = await axios.get('http://frontend-user-management-service/service-3');
-        res.json(response.data);
+        const response = await axios.get('http://users-frontend-service:3000');
+        res.send(response.data);
     } catch (error) {
-        console.error('Error al obtener datos del microservicio user management:', error);
-        res.status(500).json({ error: 'Error al obtener datos del microservicio user management' });
+        console.error('Error al obtener datos del microservicio scheduling:', error);
+        res.status(500).json({ error: 'Error al obtener datos del microservicio scheduling' });
     }
 });
+
+// Define reverse proxy middleware for each microservice
+const serviceProxyUsers = createProxyMiddleware({
+    target: 'http://users-frontend-service:3000',
+    changeOrigin: true,
+    //pathRewrite: {'^/service-users' : '/'}
+    // pathRewrite: {
+    //   '^/service-users': '/'
+    // }
+  });
+
+//   const serviceProxyUsersStatic = createProxyMiddleware({
+//     target: 'http://users-frontend-service:3000/_next',
+//     changeOrigin: true,
+//   });
+
+app.use('/service-users', serviceProxyUsers);
+//app.use('/_next', serviceProxyUsersStatic);
 
 // Escuchar en el puerto
 app.listen(PORT, () => {
