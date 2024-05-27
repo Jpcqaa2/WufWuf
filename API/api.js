8ff0fc2d830b99ac0dd3ec57cc420b4cde97b18d
Nicol <Nicol Ortiz>
2024-05-27 08:16:21 -0500

    More proxies for catalog microservice

--- API/api.js
+++ API/api.js
@@ -4,16 +4,89 @@ const axios = require('axios');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-const PORT = 3000;
+const PORT1 = 3000;
+const PORT2 = 3001;
 
-app.get('/service-catalog', async (req, res) => {
-    try {
-        const response = await axios.get('http://frontend-catalog-service/service-1');
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error al obtener datos del microservicio catalog:', error);
-        res.status(500).json({ error: 'Error al obtener datos del microservicio catalog' });
+// ================== API GATEWAY PARA EL CATÁLOGO ==================
+
+// Configura un proxy para redirigir las solicitudes a catalog-front-service
+const serviceProxyCatalog = createProxyMiddleware({
+    target: 'http://catalog-front-service:3000',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/service-pets': '', // Remueve '/service-pets' de la ruta antes de redirigir
+    },
+});
+// endpoint for the catalog microservice
+app.use('/service-pets', serviceProxyCatalog);
+
+
+// Configura un proxy para redirigir las solicitudes a catalog-front-service/service-pets/api/
+const serviceProxyCatalogPets  = createProxyMiddleware({
+    target: 'http://catalog-front-service:3000/service-pets/api/',
+    changeOrigin: true,
+});
+app.use('/api', (req, res, next) => {
+    // Pasa el cuerpo de la solicitud al proxy
+    serviceProxyCatalogPets(req, res, next);
+});
+
+
+// Configura un proxy para redirigir las solicitudes a catalog-front-service/service-pets/api/:id
+const serviceProxyCatalogPetId = createProxyMiddleware({
+    target: 'http://catalog-front-service:3000/service-pets/api/',
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+        // Obtenemos el id del parámetro de la ruta
+        const id = req.params.id;
+        // Concatenamos el id a la ruta
+        return `${path}/${id}`;
     }
+});
+// Usa el proxy para todas las solicitudes a /api/:id
+app.use('/api/:id', (req, res, next) => {
+    serviceProxyCatalogPetId(req, res, next);
+});
+
+
+// Configura un proxy para redirigir las solicitudes a catalog-front-service/service-pets/api/list_all_pets/
+const serviceProxyCatalogPetsAll  = createProxyMiddleware({
+    target: 'http://catalog-front-service:3000/service-pets/api/list_all_pets/',
+    changeOrigin: true,
+});
+app.use('/service-pets/api/list_all_pets/', (req, res, next) => {
+    // Pasa el cuerpo de la solicitud al proxy
+    serviceProxyCatalogPetsAll(req, res, next);
+});
+
+
+// Configura un proxy para redirigir las solicitudes a catalog-front-service/service-pets/api/create_pet/
+const serviceProxyCatalogPetsCreate = createProxyMiddleware({
+    target: 'http://catalog-front-service:3000/service-pets/api/create_pet/',
+    changeOrigin: true,
+});
+app.use('/api/create_pet/', (req, res, next) => {
+    // Pasa el cuerpo de la solicitud al proxy
+    serviceProxyCatalogPetsCreate(req, res, next);
+});
+
+// Inicia el servidor en el puerto PORT1
+app.listen(PORT1, () => {
+    console.log(`Servidor Express escuchando en el puerto ${PORT1}`);
+});
+
+// ================== API GATEWAY PARA EL SERVICIO DE AUTENTICACION ==================
+//Define reverse proxy middleware for each microservice
+const serviceProxyUsers = createProxyMiddleware({
+    target: 'http://users-frontend-service:3001',
+    changeOrigin: true,
+  });
+
+app.use('/service-users', serviceProxyUsers);
+
+// Escuchar en el puerto
+app.listen(PORT2, () => {
+    console.log(`Servidor Express escuchando en el puerto ${PORT2}`);
 });
 
 app.get('/service-scheduling', async (req, res) => {
@@ -36,25 +109,3 @@ app.get('/test2', async (req, res) => {
     }
 });
 
-// Define reverse proxy middleware for each microservice
-const serviceProxyUsers = createProxyMiddleware({
-    target: 'http://users-frontend-service:3000',
-    changeOrigin: true,
-    //pathRewrite: {'^/service-users' : '/'}
-    // pathRewrite: {
-    //   '^/service-users': '/'
-    // }
-  });
-
-//   const serviceProxyUsersStatic = createProxyMiddleware({
-//     target: 'http://users-frontend-service:3000/_next',
-//     changeOrigin: true,
-//   });
-
-app.use('/service-users', serviceProxyUsers);
-//app.use('/_next', serviceProxyUsersStatic);
-
-// Escuchar en el puerto
-app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
