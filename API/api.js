e5b9d4efc5b1f9a1667aa4fbaa1f51248d1233ad
Sebastian Peñaranda Hurtado <127699683+Heroslaver-tech@users.noreply.github.com>
2024-06-09 08:43:51 -0500

    add middleware

--- API/api.js
+++ API/api.js
@@ -1,38 +1,59 @@
 const express = require('express');
-const axios = require('axios');
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
+
 const PORT = 3000;
 
-app.get('/service-catalog', async (req, res) => {
-    try {
-        const response = await axios.get('http://frontend-catalog-service/service-1');
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error al obtener datos del microservicio catalog:', error);
-        res.status(500).json({ error: 'Error al obtener datos del microservicio catalog' });
-    }
+// Define reverse proxy middleware for each microservice
+const serviceProxyUsers = createProxyMiddleware({
+    target: 'http://users-frontend-service:3000',
+    changeOrigin: true,
+    //pathRewrite: {'^/service-users' : '/'}
+    // pathRewrite: {
+    //   '^/service-users': '/'
+    // }
+  });
+
+//   const serviceProxyUsersStatic = createProxyMiddleware({
+//     target: 'http://users-frontend-service:3000/_next',
+//     changeOrigin: true,
+//   });
+
+// Define reverse proxy middleware for each microservice
+const serviceProxyDates = createProxyMiddleware({
+  target: 'http://frontend-dates-service:3000',
+  changeOrigin: true,
+  //pathRewrite: {'^/service-users' : '/'}
+  // pathRewrite: {
+  //   '^/service-users': '/'
+  // }
 });
 
-app.get('/service-scheduling', async (req, res) => {
-    try {
-        const response = await axios.get('http://frontend-dates-service');
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error al obtener datos del microservicio scheduling:', error);
-        res.status(500).json({ error: 'Error al obtener datos del microservicio scheduling' });
-    }
+
+//   const serviceProxyUsersStatic = createProxyMiddleware({
+//     target: 'http://users-frontend-service:3000/_next',
+//     changeOrigin: true,
+//   });
+
+// Define reverse proxy middleware for each microservice
+const serviceProxyCatalog = createProxyMiddleware({
+  target: 'http://catalog-front-service:3000',
+  changeOrigin: true,
+  //pathRewrite: {'^/service-users' : '/'}
+  // pathRewrite: {
+  //   '^/service-users': '/'
+  // }
 });
 
-app.get('/service-user-management', async (req, res) => {
-    try {
-        const response = await axios.get('http://frontend-user-management-service/service-3');
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error al obtener datos del microservicio user management:', error);
-        res.status(500).json({ error: 'Error al obtener datos del microservicio user management' });
-    }
-});
+app.use('/service-users', serviceProxyUsers);
+//app.use('/_next', serviceProxyUsersStatic);
+
+app.use('/service-dates', serviceProxyDates);
+//app.use('/_next', serviceProxyUsersStatic);
+
+app.use('/service-pets', serviceProxyCatalog);
+//app.use('/_next', serviceProxyUsersStatic);
 
 // Escuchar en el puerto
 app.listen(PORT, () => {
